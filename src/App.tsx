71baf3f9dc2f9d2dde8c639b645c08e7566c1dc9
David Carmo <davidsc2812@gmail.com>
2021-09-03 17:19:17 -0300

    feat: criacao do footer da aplicacao

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes } from "./routes";
 import "./global.css";
 import { Header } from "./pages/Header";
+import { Footer } from "./pages/Footer";
 function App() {
   return(
   <>
   <Header />
-  <Routes />;
+  <Routes />
+  <Footer />
   </>);
 }
 
---
+++ src/Pages/Footer/index.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.scss';
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        © 2021 Grupo FCamara Formação e Consultoria <br />
+        Todos os direitos reservados.
+      </p>
+    </footer>
+  );
+};
